e424922fb799dba3db1e1d19bec220605a18b4d4
bertt <btemme@hotmail.com>
2015-04-28 17:53:04 +0200

    fixed add feature

--- PclTester.cs
+++ PclTester.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ArcGIS.ServiceModel;
@@ -10,7 +9,7 @@ namespace ArcGISPclDemo
 {
     public class PclTester
     {
-        public static async Task<int> GetEarthQuakesFromMapServer()
+        public static async Task<int> GetFeaturesFromMapServer()
         {
             var gateway = new PortalGateway("http://sampleserver3.arcgisonline.com/ArcGIS/");
             var queryPoint = new Query("Earthquakes/EarthquakesFromLastSevenDays/MapServer/0".AsEndpoint());
@@ -19,22 +18,18 @@ namespace ArcGISPclDemo
         }
 
 
-        public static async Task<int> GetEarthQuakesFromFeatureServer()
+        public static async Task<int> GetFeaturesFromFeatureServer(string BaseUrl, string ArcGISServerEndPoint)
         {
-            var gateway = new PortalGateway("http://sampleserver3.arcgisonline.com/ArcGIS/");
-            var queryPoint = new Query("/Earthquakes/EarthquakesFromLastSevenDays/FeatureServer/0".AsEndpoint());
+            var gateway = new PortalGateway(BaseUrl);
+            var queryPoint = new Query(ArcGISServerEndPoint.AsEndpoint());
             var resultPoints = await gateway.Query<Point>(queryPoint);
             return resultPoints.Features.Count();
         }
 
-        public static async Task<int> AddEarthquake()
+        public static async Task<int> AddFeature(string BaseUrl, string ArcGISServerEndPoint,Feature<Point> feature )
         {
-            var gateway = new PortalGateway("http://sampleserver3.arcgisonline.com/ArcGIS/");
-            var feature = new Feature<Point>();
-            feature.Attributes.Add("type", 0);
-            feature.Geometry = new Point { SpatialReference = new SpatialReference { Wkid = SpatialReference.WGS84.Wkid }, X = 5, Y = 53 };
-
-            var adds = new ApplyEdits<Point>(@"/Earthquakes/EarthquakesFromLastSevenDays/FeatureServer/0".AsEndpoint())
+            var gateway = new PortalGateway(BaseUrl);
+            var adds = new ApplyEdits<Point>(ArcGISServerEndPoint.AsEndpoint())
             {
                 Adds = new List<Feature<Point>> { feature }
             };
--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ArcGIS.ServiceModel;
 using ArcGIS.ServiceModel.Common;
-using ArcGIS.ServiceModel.Operation;
 
 namespace ArcGISPclDemo
 {
@@ -12,24 +7,37 @@ namespace ArcGISPclDemo
     {
         static void Main()
         {
+
+            //var baseUrl = "http://sampleserver3.arcgisonline.com/ArcGIS/";
+            //var arcGisServerEndPoint = "/Earthquakes/EarthquakesFromLastSevenDays/FeatureServer/0";
+            var baseUrl = "http://arcgis.geodan.nl:6080/arcgis/";
+            var arcGisServerEndPoint = "/public/test_AGF/FeatureServer/4";
+
             // initialize
             ArcGIS.ServiceModel.Serializers.JsonDotNetSerializer.Init();
 
-            // get something from mapserver
-            var resMapServer = PclTester.GetEarthQuakesFromMapServer();
-            Console.WriteLine("Earthquakes from Esri mapserver: {0}", resMapServer.Result);
-
             // get something from featureserver
-            var resFeatureServer = PclTester.GetEarthQuakesFromFeatureServer();
+            var resFeatureServer = PclTester.GetFeaturesFromFeatureServer(baseUrl, arcGisServerEndPoint);
             Console.WriteLine("Earthquakes from Esri featureserver: {0}", resFeatureServer.Result);
 
-            // add an earthquake
-            var res = PclTester.AddEarthquake();
-            Console.WriteLine("Added earthquakes to Esri featureserver: {0}", res.Result);
+            // add a feature
+            var feature = new Feature<Point>();
+            feature.Attributes.Add("objectid_1",0);
+            feature.Attributes.Add("objectid", 0);
+            feature.Attributes.Add("o_watergan"," ");
+            feature.Attributes.Add("symbologie"," ");
+            feature.Attributes.Add("angle",0);
+            feature.Attributes.Add("sysangle",0);
 
+            feature.Geometry = new Point { X = 165282.05719999969, Y = 501225.37609999999, SpatialReference = new SpatialReference { Wkid = 28992 } };
+
+            var res = PclTester.AddFeature(baseUrl, arcGisServerEndPoint,feature);
+            Console.WriteLine("Added feature to Esri featureserver: {0}", res.Result);
 
             Console.ReadKey();
         }
 
     }
 }
+
+
