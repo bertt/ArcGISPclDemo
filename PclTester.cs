152e4e910112d13caf702c6a27f4e7e3a70afcf6
bertt <btemme@hotmail.com>
2015-04-28 15:27:00 +0200

    refactor

---
+++ PclTester.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArcGIS.ServiceModel;
+using ArcGIS.ServiceModel.Common;
+using ArcGIS.ServiceModel.Operation;
+
+namespace ArcGISPclDemo
+{
+    public class PclTester
+    {
+        public static async Task<int> GetEarthQuakesFromMapServer()
+        {
+            var gateway = new PortalGateway("http://sampleserver3.arcgisonline.com/ArcGIS/");
+            var queryPoint = new Query("Earthquakes/EarthquakesFromLastSevenDays/MapServer/0".AsEndpoint());
+            var resultPoints = await gateway.Query<Point>(queryPoint);
+            return resultPoints.Features.Count();
+        }
+
+
+        public static async Task<int> GetEarthQuakesFromFeatureServer()
+        {
+            var gateway = new PortalGateway("http://sampleserver3.arcgisonline.com/ArcGIS/");
+            var queryPoint = new Query("/Earthquakes/EarthquakesFromLastSevenDays/FeatureServer/0".AsEndpoint());
+            var resultPoints = await gateway.Query<Point>(queryPoint);
+            return resultPoints.Features.Count();
+        }
+
+        public static async Task<int> AddEarthquake()
+        {
+            var gateway = new PortalGateway("http://sampleserver3.arcgisonline.com/ArcGIS/");
+            var feature = new Feature<Point>();
+            feature.Attributes.Add("type", 0);
+            feature.Geometry = new Point { SpatialReference = new SpatialReference { Wkid = SpatialReference.WGS84.Wkid }, X = 5, Y = 53 };
+
+            var adds = new ApplyEdits<Point>(@"/Earthquakes/EarthquakesFromLastSevenDays/FeatureServer/0".AsEndpoint())
+            {
+                Adds = new List<Feature<Point>> { feature }
+            };
+            var resultAdd = await gateway.ApplyEdits(adds);
+
+            return resultAdd.Adds.Count;
+        }
+    }
+}
